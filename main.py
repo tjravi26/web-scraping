bab3d548742d470d362cf38e4e4a824dfb42082d
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-25 13:21:49 +0530

    Updated

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ with open('home.html', 'r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
     # print(soup.prettify())  # prints the html data in a readable format.
+
     tags = soup.find('p')  # finds only the first value.
     # print(tags)
+
     courses_html_tags = soup.find_all('p')  # finds all the values.
+    for course in courses_html_tags:
+        print(course.text)
+
     # Can filter using class.
     # Must use _ after class as 'class' is a built-in method of python.
-    course_cards = soup.find_all('h4', class_='card-title')
+    course_cards = soup.find_all('div', class_='card-body')
+
     # Can even filter while printing.
     for course in course_cards:
-        print(course.text)
+        print(
+            f"The course: {course.h4.text} costs: {course.a.text.split()[-1]}")
